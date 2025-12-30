84fb022b212abea733ca0a959669659ae37dea3f
Zay269-ops <chaharanez@gmail.com>
2025-12-30 11:26:33 +0100

    Add files via upload

---
+++ films.js
@@ -0,0 +1,12 @@
+const films = [
+  { title: "Titanic", emojis: "🚢❄️❤️" },
+  { title: "Avatar", emojis: "🌍🔵🌿" },
+  { title: "Inception", emojis: "🌀💭⏱️" },
+  { title: "Interstellar", emojis: "🚀🌌⏳" },
+  { title: "Matrix", emojis: "💊🖥️🕶️" },
+  { title: "Jurassic Park", emojis: "🦖🌴🚙" },
+  { title: "Le Roi Lion", emojis: "🦁👑🌅" },
+  { title: "Forrest Gump", emojis: "🏃🍫🪑" },
+  { title: "Retour vers le futur", emojis: "⏰🚗⚡" },
+  { title: "Indiana Jones", emojis: "🤠🏺🐍" }
+];
